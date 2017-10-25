df38279318cdb43a14bfa089ade0425406afb4bb
cbornet <cbornet@hotmail.com>
2017-10-25 18:46:21 +0000

    Redirect to github.io

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ var express = require('express');
 
 var app = express();
 
+app.get('/', function(req, res, next) {
+  res.redirect('https://cbornet.github.io/netatmo-swagger-decl/swagger-ui/');
+});
+
 app.set('port', (process.env.PORT || 5000));
-app.use(express.static(__dirname + '/public'));
-app.use(express.static(__dirname + '/node_modules/swagger-ui/dist'));
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
